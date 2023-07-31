67e233a92f69dbf664c82cb8b86b16769ed77d85
thumbzzero <thumbzzero@gmail.com>
2023-07-31 14:43:10 +0900

    design: 내 체형 정보 조회 디자인 수정

--- app/user/mysize/page.tsx
+++ app/user/mysize/page.tsx
@@ -1,3 +1,4 @@
+import EditButton from '@/components/MyPage/MySize/EditButton';
 import MySizeInfo from '@/components/MyPage/MySize/MySizeInfo';
 import React from 'react';
 import MyPageSection from '../../../components/MyPage/MyPageSection';
@@ -9,11 +10,12 @@ function MySizePage(props: Props) {
   return (
     <>
       <MyPageSection sectionName="내 맞춤 정보 설정" />
-      <div className="flex flex-col gap-8 md:gap-4 w-full md:flex-row">
+      <div className="flex flex-col gap-8 md:gap-12 w-full md:flex-row">
         <GuidelinePic />
         {/* @ts-expect-error Server Component */}
         <MySizeInfo />
       </div>
+      <EditButton />
     </>
   );
 }
--- components/MyPage/MySize/GuidelinePic.tsx
+++ components/MyPage/MySize/GuidelinePic.tsx
@@ -9,7 +9,7 @@ export default function GuidelinePic() {
   return (
     <div className="flex-1 flex flex-col w-full">
       <div className="font-semibold pb-6">가이드라인 사진 예시</div>
-      <div className="flex gap-4 w-full h-72 md:h-96 items-center justify-center">
+      <div className="flex gap-4 w-full h-72 md:h-[22rem] items-center justify-center">
         <Image className={guidelinePicStyle} src={guideline1} alt="" />
         <Image className={guidelinePicStyle} src={guideline2} alt="" />
       </div>
