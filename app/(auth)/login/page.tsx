0d31b42419ba494fb72cebb1a0eea1324477e139
thumbzzero <thumbzzero@gmail.com>
2023-08-09 19:20:57 +0900

    design: 로그인 페이지 css 수정

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -10,14 +10,14 @@ type Props = {};
 
 function LoginPage(props: Props) {
   return (
-    <div className="flex text-center mt-8 px-72 justify-center items-center max-h-min">
-      <div className="md:max-xl:hidden flex-1">
-        <Image className="p-16" src={phone} alt="phone" />
+    <div className="flex flex-grow gap-12 text-center justify-center items-center min-h-[calc(100vh-1rem)] ml-auto mr-auto">
+      <div className="max-w-[330px] [@media(max-width:900px)]:hidden flex-1">
+        <Image className="" src={phone} alt="phone" />
       </div>
-      <div className="flex-1">
-        <div className="border mb-4 p-10">
+      <div className="flex-1 max-w-[400px]">
+        <div className="border mb-4 p-10 [@media(max-width:400px)]:border-none">
           <h1 className="text-3xl font-bold mt-6">
-            <Image className="w-1/3 block m-auto" src={logo} alt="Google" />
+            <Image className="w-1/2 xs:w-1/3 block m-auto" src={logo} alt="Fittering" />
           </h1>
           <LoginForm />
           <div className="flex my-8">
@@ -30,7 +30,7 @@ function LoginPage(props: Props) {
             비밀번호를 잊으셨나요?
           </Link>
         </div>
-        <div className="border p-6">
+        <div className="border p-6 [@media(max-width:400px)]:border-none">
           계정이 없으신가요?{' '}
           <Link className="text-main-color font-bold" href="/signup">
             가입하기
