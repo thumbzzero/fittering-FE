dea80eebe02206df4d1a1915ce4d93d0db254dca
thumbzzero <thumbzzero@gmail.com>
2023-08-15 15:01:44 +0900

    refactor: 메인 페이지 섹션 별 컴포넌트 렌더링

--- app/(service)/page.tsx
+++ app/(service)/page.tsx
@@ -1,16 +1,14 @@
 import GlobalFilter from '@/components/GlobalFilter';
-import RankedMallPreview from '@/components/Malls/RankedMallPreview';
-import Section from '@/components/Section';
 import GlobalMenu from '@/components/GlobalMenu';
+import MallRanking from '@/components/MainPage/MallRanking';
+import ProductRecommendation from '@/components/MainPage/ProductRecommendation';
 
 export default function Home() {
   return (
     <section>
-      <Section sectionName="자주 방문한 쇼핑몰" />
-      {/* @ts-expect-error Server Component */}
-      <RankedMallPreview />
-      <Section sectionName="열졍콩님을 위한 추천 상품" />
-      <div className="block bg-bean-head-color w-full h-[1000vh]"></div>
+      <MallRanking />
+      <ProductRecommendation recommendationType={1} />
+      <ProductRecommendation recommendationType={2} />
       <GlobalFilter />
       <GlobalMenu />
     </section>
---
+++ components/MainPage/MallRanking.tsx
@@ -0,0 +1,11 @@
+import Section from '@/components/Section';
+import RankedMallPreview from '@/components/Malls/RankedMallPreview';
+
+export default function MallRanking() {
+  return (
+    <>
+      <Section sectionName="자주 방문한 쇼핑몰" />
+      <RankedMallPreview />
+    </>
+  );
+}
