ad60ae19ee1c55377f6d7563bb276a0cdce64b02
thumbzzero <thumbzzero@gmail.com>
2023-08-16 16:57:30 +0900

    fix: 마이페이지 섹션 전체 버튼 추가

--- components/MyPage/MyPageSection.tsx
+++ components/MyPage/MyPageSection.tsx
@@ -1,12 +1,26 @@
+import Link from 'next/link';
+
 type Props = {
   sectionName: '최근 본 상품' | '좋아요한 상품' | '내 맞춤 정보 설정';
   displayStyle: string;
 };
 
 export default function MyPageSection({ sectionName, displayStyle }: Props) {
+  const href =
+    sectionName === '최근 본 상품' ? '/user/recent' : '/favorite_goods';
   return (
-    <div className={`my-8 pb-2 border-b-[1.5px] border-nav-black ${displayStyle}`}>
+    <div
+      className={`relative mt-8 pb-2 border-b-[1.5px] border-nav-black ${displayStyle}`}
+    >
       <span className="font-bold text-lg">{sectionName}</span>
+      {sectionName !== '내 맞춤 정보 설정' ? (
+        <Link
+          className="absolute right-0 bottom-[0.5rem] text-xs text-custom-gray-400"
+          href={`${href}`}
+        >
+          {'전체 >'}
+        </Link>
+      ) : null}
     </div>
   );
 }
