59680e9699fa853e5d1835bae4befb6bcf22a7b6
thumbzzero <thumbzzero@gmail.com>
2023-08-19 17:20:03 +0900

    fix: 쇼핑몰 상세 조회 링크 href 수정

--- components/Malls/MallPreviewCard.tsx
+++ components/Malls/MallPreviewCard.tsx
@@ -10,7 +10,7 @@ export default function MallPreview({ mall: { name, image } }: Props) {
   return (
     <div className="text-center">
       <div className="w-24 h-24 md:w-44 md:h-36 mx-auto mb-2">
-        <Link href={`/malls/${name}`}>
+        <Link href={`/malls/${name.split(' ').join('').toLowerCase()}`}>
           <Image
             className="w-full h-full p-2 md:p-4 rounded-lg object-contain mx-auto border border-custom-gray-100"
             src={image}
@@ -20,7 +20,10 @@ export default function MallPreview({ mall: { name, image } }: Props) {
           />
         </Link>
       </div>
-      <Link className="font-semibold text-sm" href={`/malls/${name}`}>
+      <Link
+        className="font-semibold text-sm"
+        href={`/malls/${name.split(' ').join('').toLowerCase()}`}
+      >
         {name}
       </Link>
     </div>
