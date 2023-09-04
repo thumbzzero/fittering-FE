3c34dfb47989567d3701829cdf25373d47a760c7
thumbzzero <thumbzzero@gmail.com>
2023-09-04 20:58:22 +0900

    fix: 쇼핑몰 관련 타입 수정

--- service/malls.ts
+++ service/malls.ts
@@ -1,21 +1,32 @@
 import { customFetch } from '@/utils/customFetch';
 
+export type MallRankingPreview = {
+  id: number;
+  name: string;
+  image: string;
+};
+
 export type MallPreview = {
   id: number;
   name: string;
   image: string;
+  description: string;
+  view: number;
+  products: {
+    productId: number;
+    productImage: string;
+  }[];
 };
 
 export type Mall = {
   id: number;
   name: string;
   url: string;
-  description: string;
   image: string;
-  view: number;
+  description: string;
 };
 
-export async function getRankedMallPreview(): Promise<MallPreview[]> {
+export async function getRankedMallPreview(): Promise<MallRankingPreview[]> {
   const response = await customFetch('/malls/rank/preview');
   if (!response.ok) {
     return [];
