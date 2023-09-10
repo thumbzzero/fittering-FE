1bd98dd192d7fceb58d1ded37fc961e77ec5fa10
thumbzzero <thumbzzero@gmail.com>
2023-09-10 17:10:42 +0900

    feat: 상품 카테고리별 사이즈 타입 정의

---
+++ types/sizes.ts
@@ -0,0 +1,30 @@
+export type TopSize = {
+  name?: string;
+  full?: number;
+  shoulder?: number;
+  chest?: number;
+  sleeve?: number;
+};
+
+export type BottomSize = {
+  name?: string;
+  full?: number;
+  waist?: number;
+  thigh?: number;
+  rise?: number;
+  bottomWidth?: number;
+  hipWidth?: number;
+};
+
+export type DressSize = {
+  name?: string;
+  full?: number;
+  shoulder?: number;
+  chest?: number;
+  waist?: number;
+  armHall?: number;
+  hip?: number;
+  sleeve?: number;
+  sleeveWidth?: number;
+  bottomWidth?: number;
+};
