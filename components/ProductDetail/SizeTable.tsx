5a0c5dac92bcebfec711af12ab96c8178ba80dd2
thumbzzero <thumbzzero@gmail.com>
2023-09-12 19:28:22 +0900

    fix: 유효하지 않은 사이즈 정보 사이즈표에 나타나지 않도록 수정

--- components/ProductDetail/SizeTable.tsx
+++ components/ProductDetail/SizeTable.tsx
@@ -18,19 +18,31 @@ export default function SizeTable({ sizeData }: Props) {
         <tr>
           {sizeData.map((size, index) => {
             if (index === 0) {
-              return size.map((info, index) => (
-                <th key={index}>{info.label}</th>
-              ));
+              return size.map((info, index) => {
+                if (info.data !== undefined) {
+                  return (
+                    <th key={index}>
+                      {info.label}
+                    </th>
+                  );
+                }
+              });
             }
           })}
         </tr>
       </thead>
       <tbody>
-        {sizeData.map((size, index) => (
+        {sizeData?.map((size, index) => (
           <tr key={index}>
-            {size.map((info, index) => (
-              <td key={index}>{info.data}</td>
-            ))}
+            {size.map((info, index) => {
+              if (info.data !== undefined) {
+                return (
+                  <td key={index}>
+                    {info.data}
+                  </td>
+                );
+              }
+            })}
           </tr>
         ))}
       </tbody>
