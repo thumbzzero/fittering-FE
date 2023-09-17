a4f767f7ebe6f8614b7aa984b3759d320ba72e8e
thumbzzero <thumbzzero@gmail.com>
2023-09-17 15:42:53 +0900

    feat: 상품 상세 정보 txt 파일 렌더링

--- components/ProductDetail/DetailText.tsx
+++ components/ProductDetail/DetailText.tsx
@@ -1,7 +1,29 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
 type Props = {
   url: string;
-}
+};
 
 export default function DetailText({ url }: Props) {
-  return <></>;
+  const [text, setText] = useState<string[]>([]);
+  useEffect(() => {
+    const fetchTextFile = async () => {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.text();
+        setText(data.split('\n'));
+      }
+    };
+    fetchTextFile();
+  }, [url]);
+
+  return (
+    <div className="my-6 text-center">
+      {text.map((line, index) => (
+        <p key={index}>{line}</p>
+      ))}
+    </div>
+  );
 }
