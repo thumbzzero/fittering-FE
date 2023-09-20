5d104979a1845c5763eca422f5d551ded2369041
thumbzzero <thumbzzero@gmail.com>
2023-09-20 18:28:07 +0900

    feat: 체형 실루엣 이미지 제공 기능 추가

--- components/MyPage/MySize/SmartAnalysisPhotoSubmitForm.tsx
+++ components/MyPage/MySize/SmartAnalysisPhotoSubmitForm.tsx
@@ -1,19 +1,65 @@
+'use client';
+
+import { getSilhouetteImage } from '@/service/mysize';
 import Image from 'next/image';
 import plus from 'public/icon/plus_white.svg';
+import { useRef, useState } from 'react';
 
 type Props = {
   photoType: 'FRONT' | 'SIDE';
 };
 
 export default function SmartAnalysisPhotoSubmitForm({ photoType }: Props) {
+  const imageInput = useRef<HTMLInputElement>(null);
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+
+  const handleImageInputClick = () => {
+    imageInput.current?.click();
+  };
+
+  const handleFileUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const file = e.target.files?.[0];
+    if (file) {
+      const formData = new FormData();
+      formData.append('bodyFile', file);
+      const imageUrl = await getSilhouetteImage(photoType, formData);
+      setImageUrl(imageUrl);
+    }
+  };
+
   return (
-    <div className="flex flex-col gap-4 items-center justify-center border rounded-lg w-full h-[54vh] text-center">
-      <button className="bg-main-color text-white text-[3rem] rounded-full w-16 h-16">
-        <Image className="mx-auto" src={plus} alt="+" />
-      </button>
-      <span className="text-main-color">
-        {photoType === 'FRONT' ? '정면' : '측면'} 사진 등록
-      </span>
+    <div
+      onClick={handleImageInputClick}
+      className="flex flex-col gap-4 items-center justify-center border rounded-lg w-full h-[54vh] text-center"
+    >
+      {imageUrl === null ? (
+        <>
+          <form encType="multipart/form-data">
+            <input
+              className="hidden"
+              type="file"
+              accept=".jpg, .jpeg, .png"
+              ref={imageInput}
+              onChange={handleFileUpload}
+            />
+          </form>
+          <button className="bg-main-color text-white text-[3rem] rounded-full w-16 h-16">
+            <Image className="mx-auto" src={plus} alt="+" />
+          </button>
+          <span className="text-main-color">
+            {photoType === 'FRONT' ? '정면' : '측면'} 사진 등록
+          </span>
+        </>
+      ) : (
+        <Image
+          className="w-full h-full p-4 rounded-sm object-contain"
+          src={imageUrl}
+          alt="실루엣 사진"
+          width={200}
+          height={200}
+        />
+      )}
     </div>
   );
 }
--- service/mysize.ts
+++ service/mysize.ts
@@ -1,3 +1,4 @@
+import { customFetch } from '@/utils/customFetch';
 import { readFile } from 'fs/promises';
 import path from 'path';
 
@@ -17,4 +18,23 @@ export async function getMySize(): Promise<MySize> {
   // 더미 데이터 읽어옴
   const filePath = path.join(process.cwd(), 'data', 'mysize.json');
   return readFile(filePath, 'utf-8').then<MySize>(JSON.parse);
+}
+
+export async function getSilhouetteImage(
+  type: 'FRONT' | 'SIDE',
+  formData: FormData
+): Promise<string | null> {
+  const response = await customFetch(
+    `/users/mysize/silhouette?type=${type}`,
+    {
+      method: 'POST',
+      body: formData,
+    },
+    true
+  );
+  if (!response.ok) {
+    return null;
+  }
+  const data: { url: string } = await response.json();
+  return data.url;
 }
