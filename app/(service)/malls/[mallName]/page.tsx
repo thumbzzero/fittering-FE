89850d915eadf97878c57000f9efc4bb8ad8ec9f
thumbzzero <thumbzzero@gmail.com>
2023-09-22 15:41:02 +0900

    feat: 쇼핑몰 상세 페이지 카테고리별 상품 렌더링

--- app/(service)/malls/[mallName]/page.tsx
+++ app/(service)/malls/[mallName]/page.tsx
@@ -5,6 +5,8 @@ import { mallNameToMallId } from '@/utils/mallNameToMallId';
 import MallIntroduction from '@/components/Mall/MallIntroduction';
 import MainCategoryNavbar from '@/components/Navbar/MainCategoryNavbar';
 import SubCategoryNavbar from '@/components/Navbar/SubCategoryNavbar';
+import { getCategoriesProductsData } from '@/service/categoriesProducts';
+import CategoryProducts from '@/components/Products/CategoryProducts';
 
 type Props = {
   params: {
@@ -12,13 +14,28 @@ type Props = {
   };
 };
 
-function MallPage({ params: { mallName } }: Props) {
+export const revalidate = 1209600; // 3600*24*14
+
+async function MallPage({ params: { mallName } }: Props) {
   const mallId = mallNameToMallId(decodeURI(mallName));
+  const mallMainCategoriesProducts = await getCategoriesProductsData(
+    'main',
+    await mallId
+  );
+  const mallSubCategoriesProducts = await getCategoriesProductsData(
+    'sub',
+    await mallId
+  );
+
   return (
     <div>
       <MallIntroduction mallId={mallId} />
       <MainCategoryNavbar mallName={decodeURI(mallName)} />
       <SubCategoryNavbar mallName={decodeURI(mallName)} />
+      <CategoryProducts
+        mainCategoriesProducts={mallMainCategoriesProducts}
+        subCategoriesProducts={mallSubCategoriesProducts}
+      />
       <GlobalFilter />
       <GlobalMenu />
     </div>
