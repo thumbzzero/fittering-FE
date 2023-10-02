9d87edc3ba82066588d264997157783c8e9c3bf5
thumbzzero <thumbzzero@gmail.com>
2023-10-02 20:28:53 +0900

    feat: 유저 관련 페이지에 스토어 제공

---
+++ app/(service)/user/layout.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import store from '@/store/store';
+import { Provider } from 'react-redux';
+
+export default function MyPageLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Provider store={store}>{children}</Provider>
+    </>
+  );
+}
