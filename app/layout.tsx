9e572547f633fb9e9ce8dc1e46122ac591866a8a
thumbzzero <thumbzzero@gmail.com>
2023-10-14 15:14:57 +0900

    fix: title, description 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,9 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Fittering',
-  description: 'fit + filtering',
+  title: '핏터링 - Fittering',
+  description:
+    '핏터링(Fittering, Fit + Filtering)을 통해 체형 분석을 쉽게 하고, 나의 체형에 fit한 옷을 구매해보세요!',
 };
 
 export default function RootLayout({
