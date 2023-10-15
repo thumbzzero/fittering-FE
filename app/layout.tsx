9e84f6117c883956ec7801a877ffc1f219f1cb7a
thumbzzero <thumbzzero@gmail.com>
2023-10-15 15:57:22 +0900

    feat: meta 태그 keywords 속성 작성

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,20 @@ export const metadata: Metadata = {
   title: '핏터링 - Fittering',
   description:
     '핏터링(Fittering, Fit + Filtering)을 통해 체형 분석을 쉽게 하고, 나의 체형에 fit한 옷을 구매해보세요!',
+  keywords: [
+    '핏터링',
+    'fittering',
+    'fit',
+    '체형',
+    '분석',
+    '쇼핑몰',
+    '옷',
+    '쇼핑',
+    '스마트분석',
+    '체형분석',
+    '추천',
+    '상품',
+  ],
 };
 
 export default function RootLayout({
