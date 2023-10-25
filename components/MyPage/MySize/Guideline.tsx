81846cdb8c9a8c6f8850a36b08a8e9aba740fd8a
thumbzzero <thumbzzero@gmail.com>
2023-10-25 20:56:08 +0900

    feat: 스마트 분석 사진 가이드라인 추가

--- components/MyPage/MySize/Guideline.tsx
+++ components/MyPage/MySize/Guideline.tsx
@@ -1,12 +1,38 @@
-const guidelineStyle = 'block py-1';
+import { guidelines } from '@/constants/guidelines';
+import Image from 'next/image';
+import frontGuideline from '/public/front_guideline.jpeg';
+import sideGuideline from '/public/side_guideline.jpeg';
 
 export default function Guideline() {
   return (
     <div>
-      <h3 className="mt-8 mb-2 font-semibold">사진 가이드라인</h3>
-      <span className={guidelineStyle}>‧ 가이드라인 설명 1</span>
-      <span className={guidelineStyle}>‧ 가이드라인 설명 2</span>
-      <span className={guidelineStyle}>‧ 가이드라인 설명 3</span>
+      <h3 className="mt-8 font-semibold text-base">사진 가이드라인</h3>
+      <div className="flex flex-col md:flex-row md:justify-start my-4 gap-4">
+        <div className="flex-1 grid grid-cols-2 gap-2 md:flex-0">
+          <Image
+            className="rounded"
+            src={frontGuideline}
+            alt="정면 사진 촬영 가이드라인"
+          />
+          <Image
+            className="rounded"
+            src={sideGuideline}
+            alt="측면 사진 촬영 가이드라인"
+          />
+        </div>
+
+        <ul className="list-disc mx-4 md:mx-8">
+          {guidelines.map((guideline, index) => (
+            <li
+              key={index}
+              className={`py-1 text-sm ${
+                guideline.isImportant && 'text-red-500'
+              }`}
+            >{`${guideline.text}`}</li>
+          ))}
+        </ul>
+        <div className="flex-1" aria-hidden={true}></div>
+      </div>
     </div>
   );
 }
---
+++ constants/guidelines.ts
@@ -0,0 +1,22 @@
+export const guidelines = [
+  {
+    text: 'JPG, PNG 이미지 파일로 등록해주세요.',
+    isImportant: false,
+  },
+  {
+    text: '신체가 장애물에 가려지지 않게 촬영해주세요.',
+    isImportant: false,
+  },
+  {
+    text: '전신이 다 보이게 촬영해주세요.',
+    isImportant: false,
+  },
+  {
+    text: '펑퍼짐한 옷을 입고 촬영하면 정확한 값을 얻기 어렵습니다.',
+    isImportant: false,
+  },
+  {
+    text: '데이터베이스에는 실루엣만 저장합니다.',
+    isImportant: true,
+  },
+];
