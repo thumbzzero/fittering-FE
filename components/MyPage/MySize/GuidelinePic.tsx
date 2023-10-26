6e3919049187837e3070682140b1d01034e605ab
thumbzzero <thumbzzero@gmail.com>
2023-10-26 16:20:04 +0900

    fix: 가이드라인 영역 문구 수정

--- components/MyPage/MySize/GuidelinePic.tsx
+++ components/MyPage/MySize/GuidelinePic.tsx
@@ -4,7 +4,7 @@ import guideline from '/public/guideline.png';
 export default function GuidelinePic() {
   return (
     <div className="flex-1 flex flex-col w-full">
-      <div className="font-semibold pb-6">가이드라인 사진 예시</div>
+      <div className="font-semibold pb-6">체형 정보 가이드라인</div>
       <Image
           className='w-auto h-full border rounded-lg overflow-hidden object-contain'
           src={guideline}
