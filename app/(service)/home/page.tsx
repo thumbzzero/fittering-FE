de9218473a6d56adfa4b0c91496599e7dcc9fff8
thumbzzero <thumbzzero@gmail.com>
2023-10-30 22:08:45 +0900

    fix: 메인 페이지 username 수정

--- app/(service)/home/page.tsx
+++ app/(service)/home/page.tsx
@@ -2,14 +2,17 @@ import GlobalFilter from '@/components/GlobalFilter';
 import GlobalMenu from '@/components/GlobalMenu';
 import MallRanking from '@/components/MainPage/MallRanking';
 import ProductRecommendation from '@/components/MainPage/ProductRecommendation';
+import ProductRecommendations from '@/components/MainPage/ProductRecommendations';
 import ProductsRank from '@/components/MainPage/ProductsRank';
 
 export default function Home() {
   return (
     <section>
       <MallRanking />
-      <ProductRecommendation recommendationType={1} />
-      <ProductRecommendation recommendationType={2} />
+      <ProductRecommendations>
+        <ProductRecommendation recommendationType={1} />
+        <ProductRecommendation recommendationType={2} />
+      </ProductRecommendations>
       <ProductsRank />
       <GlobalFilter />
       <GlobalMenu />
