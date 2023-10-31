c0f0cc5914ee0ee2f441aaed3e6f81e7f38e6e59
thumbzzero <thumbzzero@gmail.com>
2023-10-31 15:28:01 +0900

    feat: 상품 상세 페이지에 글로벌 메뉴 추가

--- app/(service)/products/[productId]/page.tsx
+++ app/(service)/products/[productId]/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import GlobalMenu from '@/components/GlobalMenu';
 import MobileSection from '@/components/MobileSection';
 import RecentProducts from '@/components/MyPage/RecentProducts';
 import DetailInfo from '@/components/ProductDetail/DetailInfo';
@@ -36,6 +37,7 @@ function ProductPage({ params: { productId } }: Props) {
       <SizeInfo category={product.category} sizes={product.sizes} />
       <DetailInfo descriptions={product.descriptions} />
       <RecentProducts isPreview={true} />
+      <GlobalMenu />
     </div>
   );
 }
