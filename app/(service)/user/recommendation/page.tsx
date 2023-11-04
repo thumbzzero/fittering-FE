13e4b344119a15d12855064eaf18e0b05cfba5c0
thumbzzero <thumbzzero@gmail.com>
2023-11-04 20:17:10 +0900

    feat: 추천 상품 전체 보기 추가

--- app/(service)/user/recommendation/page.tsx
+++ app/(service)/user/recommendation/page.tsx
@@ -1,9 +1,51 @@
-import React from 'react';
+'use client';
+
+import Section from '@/components/Section';
+import { useAppSelector } from '@/store/hooks';
+import { MyPage } from '@/types/user';
+import React, { useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { ProductPreview, getProductPreview } from '@/service/product';
+import ProductsGrid from '@/components/Products/ProductsGrid';
 
 type Props = {};
 
 function RecommendationPage(props: Props) {
-  return <div>상품 추천 페이지</div>;
+  const myPage: MyPage = useAppSelector((state) => state.myPage);
+  const searchParam = useSearchParams();
+  const recommendationType = parseInt(searchParam.get('type') ?? '1');
+
+  const [products, setProducts] = useState<ProductPreview[]>([]);
+
+  const sectionName =
+    recommendationType === 1
+      ? `${myPage.username}님을 위한 추천 상품`
+      : `${myPage.username}님과 비슷한 체형의 고객의 pick`;
+
+  useEffect(() => {
+    async function fetchProducts() {
+      const data = await getProductPreview(
+        `/users/recommendation${recommendationType === 2 ? '/pick' : ''}`
+      );
+      setProducts(data);
+    }
+    fetchProducts();
+  }, [recommendationType, products]);
+
+  if (products.length === 0) {
+    return (
+      <div className="mt-8 text-center text-custom-gray-800">
+        추천 상품이 없습니다.
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <Section sectionName={sectionName} />
+      <ProductsGrid products={products} />
+    </div>
+  );
 }
 
 export default RecommendationPage;
