03d51fa599f7010f4cdb682c6cfb7ade3cb7ce0f
thumbzzero <thumbzzero@gmail.com>
2023-11-11 20:58:50 +0900

    feat: 쇼핑몰 랭킹/즐겨찾기 전체보기에 쇼핑몰 전체 보기 링크 추가

--- app/(service)/malls/page.tsx
+++ app/(service)/malls/page.tsx
@@ -1,3 +1,4 @@
+import LinkToMallsList from '@/components/Malls/LinkToMallsList';
 import MallPreviewFilter from '@/components/Malls/MallPreviewFilter';
 import MallsList from '@/components/Malls/MallsList';
 import React from 'react';
@@ -8,6 +9,7 @@ function MallsPage(props: Props) {
   return (
     <div>
       <MallPreviewFilter isRanking={true} />
+      <LinkToMallsList />
       <MallsList isRanking={true} />
     </div>
   );
