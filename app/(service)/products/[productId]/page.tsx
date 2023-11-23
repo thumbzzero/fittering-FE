59c84656a7cd839b114cf4250d1861ff238df102
thumbzzero <thumbzzero@gmail.com>
2023-11-23 16:21:33 +0900

    refactor: 상품 상세 내용 컴포넌트 분리 후 페이지 서버 컴포넌트로 변경

--- app/(service)/products/[productId]/page.tsx
+++ app/(service)/products/[productId]/page.tsx
@@ -1,14 +1,4 @@
-'use client';
-
-import GlobalMenu from '@/components/GlobalMenu';
-import MobileSection from '@/components/MobileSection';
-import RecentProducts from '@/components/MyPage/RecentProducts';
-import DetailInfo from '@/components/ProductDetail/DetailInfo';
-import ProductSummary from '@/components/ProductDetail/ProductSummary';
-import SizeInfo from '@/components/ProductDetail/SizeInfo';
-import Section from '@/components/Section';
-import { getProduct, Product } from '@/service/product';
-import React, { useEffect, useState } from 'react';
+import ProductDetails from "@/components/ProductDetail/ProductDetails";
 
 type Props = {
   params: {
@@ -17,27 +7,9 @@ type Props = {
 };
 
 function ProductPage({ params: { productId } }: Props) {
-  const [product, setProduct] = useState<Product>({} as Product);
-
-  useEffect(() => {
-    async function fetchProduct() {
-      const data = await getProduct(parseInt(productId));
-      setProduct(data);
-    }
-    fetchProduct();
-  }, [productId]);
-
   return (
-    <div className='mb-8 md:mb-20'>
-      <div className="hidden md:block">
-        <Section sectionName={product.productName} />
-      </div>
-      <MobileSection sectionName={product.productName} />
-      <ProductSummary product={product} />
-      <SizeInfo category={product.category} sizes={product.sizes} />
-      <DetailInfo descriptions={product.descriptions} />
-      <RecentProducts isPreview={true} />
-      <GlobalMenu />
+    <div className='mb-8'>
+      <ProductDetails productId={parseInt(productId)} />
     </div>
   );
 }
---
+++ components/ProductDetail/ProductDetails.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import GlobalMenu from '@/components/GlobalMenu';
+import MobileSection from '@/components/MobileSection';
+import RecentProducts from '@/components/MyPage/RecentProducts';
+import DetailInfo from '@/components/ProductDetail/DetailInfo';
+import ProductSummary from '@/components/ProductDetail/ProductSummary';
+import SizeInfo from '@/components/ProductDetail/SizeInfo';
+import Section from '@/components/Section';
+import { getProduct, Product } from '@/service/product';
+import React, { useEffect, useState } from 'react';
+
+type Props = {
+  productId: number;
+};
+
+export default function ProductDetails({ productId }: Props) {
+  const [product, setProduct] = useState<Product>({} as Product);
+
+  useEffect(() => {
+    async function fetchProduct() {
+      const data = await getProduct(productId);
+      setProduct(data);
+    }
+    fetchProduct();
+  }, [productId]);
+
+  return (
+    <>
+      <div className="hidden md:block">
+        <Section sectionName={product.productName} />
+      </div>
+      <MobileSection sectionName={product.productName} />
+      <ProductSummary product={product} />
+      <SizeInfo category={product.category} sizes={product.sizes} />
+      <DetailInfo descriptions={product.descriptions} />
+      <RecentProducts isPreview={true} />
+      <GlobalMenu />
+    </>
+  );
+}
